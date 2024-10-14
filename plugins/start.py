23a655c002cfe0edca7342c2d576e14afce6050c
HRBsupport <wakeoneteam@gmail.com>
2024-10-14 22:32:34 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -152,6 +152,22 @@ async def not_joined(client: Client, message: Message):
         disable_web_page_preview = True
     )
 
+    @Bot.on_message(filters.command('ch2l') & filters.private)
+async def gen_link_encoded(client: Bot, message: Message):
+    try:
+        hash = await client.ask(text="Enter the code here... \n /cancel to cancel the operation",chat_id = message.from_user.id, timeout=60)
+    except Exception as e:
+        print(e)
+        await hash.reply(f"ꜱᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ⚠️{e}")
+        return
+    if hash.text == "/cancel":
+        await hash.reply("ᴄᴀɴᴄᴇʟʟᴇᴅ!")
+        return
+    link = f"https://t.me/{client.username}?start={hash.text}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("➡️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ", url=link)]])
+    await hash.reply_text(f"<b>🧑‍💻 ʜᴇʀᴇ ɪꜱ ʏᴏᴜʀ ɢᴇɴᴇʀᴀᴛᴇᴅ ʟɪɴᴋ", quote=True, reply_markup=reply_markup)
+    return
+
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
